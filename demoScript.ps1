9bb90274ca73103edb082a77839513e0ae17ae75
Bart Delanghe <bart.delanghe@microsoft.com>
2020-07-09 18:33:40 +0200

    First Deployment

--- demoScript.ps1
+++ demoScript.ps1
@@ -1,6 +1,13 @@
+
+az login
+
 cd config
 
 $tfdir="../deploy/terraform"
 
 terraform init $tfdir
-terraform plan --var-file singleHANA.json $tfdir
+terraform plan --var-file=singleHANA.json $tfdir
+terraform apply --var-file=singleHANA.json  --auto-approve $tfdir
+
+
+terrafrom destroy --var-file=singleHANA.json --auto-approve $tfdir
