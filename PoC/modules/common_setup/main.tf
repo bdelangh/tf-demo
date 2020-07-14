c3556a9892361d9b1fc33b2d08c75f271deea36a
Bart Delanghe <badelang@microsoft.com>
2020-07-14 13:19:42 +0200

    Removed quoates

--- PoC/modules/common_setup/main.tf
+++ PoC/modules/common_setup/main.tf
@@ -27,6 +27,6 @@ resource "azurerm_subnet" "subnet" {
   name                      = "hdb-subnet"
   resource_group_name       = azurerm_resource_group.hana-resource-group.name
   virtual_network_name      = azurerm_virtual_network.vnet.name
-  address_prefix            = "${var.hdb_subnet_address_space}"
+  address_prefix            = ${var.hdb_subnet_address_space}
   network_security_group_id = azurerm_network_security_group.sap_nsg[0].id
 }
